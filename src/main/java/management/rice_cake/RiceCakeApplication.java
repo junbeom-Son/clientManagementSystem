c6650e595c47c3a18d96a5ccf972422ad20720d7
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-12-26 23:53:57 +0900

    Initial commit

---
+++ src/main/java/management/rice_cake/RiceCakeApplication.java
@@ -0,0 +1,13 @@
+package management.rice_cake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/management/rice_cake/RiceCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package management.rice_cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiceCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
